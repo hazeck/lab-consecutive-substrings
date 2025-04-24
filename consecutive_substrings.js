fad7581f9b2556e8107e9125a577ab03b01a9e2e
Chadrick <chadrickalvarez@gmail.com>
2025-04-24 16:01:27 -0400

    lab

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,9 +1,18 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  const result = [];
+
+  for (let start = 0; start < string.length; start++) {
+    let substring = '';
+    for (let end = start; end < string.length; end++) {
+      substring += string[end];
+      result.push(substring);
+    }
+  }
+
+  return result;
 }
 
 if (require.main === module) {
-  // add your own tests in here
   console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
   console.log("=>", consecutiveSubstrings('abc'));
 
@@ -11,6 +20,11 @@ if (require.main === module) {
 
   console.log("Expecting: ['a']");
   console.log("=>", consecutiveSubstrings('a'));
+
+  console.log("");
+
+  console.log("Expecting: ['p','ph','pho','phon','phone','h','ho','hon','hone','o','on','one','n','ne','e']");
+  console.log("=>", consecutiveSubstrings('phone'));
 }
 
 module.exports = consecutiveSubstrings;
